b82b71c92213f9df7bbf0455242b80a104d1c241
Rahman Adewale Hafeez <superdwale@gmail.com>
2018-01-19 18:13:45 +0100

    Worked on the square and root buttons and performed an HTTP GET to send and get data from the backend

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+// import {operators} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-calculator',
@@ -8,51 +9,131 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 })
 export class CalculatorComponent implements OnInit {
   operation: string[] = ['', '', ''];
-  display: string = '';
+  display: string;
   // subDisplay:string = ''
-  activeBuildingNumber: string = '';
+  activeBuildingNumber: string;
+
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    /*this.display = '';
+    this.display = '';
     this.activeBuildingNumber = '';
-    this.operation = ['', '', ''];*/
-    interface ServerResponse {
-      login: string;
-      bio: string;
-      company: string;
-    }
-
-    this.http.get <ServerResponse>
-    ('https://api.github.com/users/seeschweiler').subscribe(data => {
-        console.log('User Login: ' + data.login);
-        console.log('Bio: ' + data.bio);
-        console.log('Company: ' + data.company);
-      },
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) {
-          console.log('Client-side error occurred.');
-        } else {
-          console.log('Server-side error occurred.');
-        }
-      }
-    );
   }
+  /*this.display = '';
+  this.activeBuildingNumber = '';
+  this.operation = ['', '', ''];*/
+  /* interface ServerResponse {
+     login: string;
+     bio: string;
+     company: string;
+   }*/
+
+  /*this.http.get <ServerResponse>
+  ('https://api.github.com/users/seeschweiler').subscribe(data => {
+      console.log('User Login: ' + data.login);
+      console.log('Bio: ' + data.bio);
+      console.log('Company: ' + data.company);
+    },
+    (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        console.log('Client-side error occurred.');
+      } else {
+        console.log('Server-side error occurred.');
+      }
+    }
+  );*/
+  /* const req = this.http.post('http://jsonplaceholder.typicode.com/posts', {
+     title: 'foo',
+     body: 'bar',
+     userId: 1
+   })
+     .subscribe(
+       res => {
+         console.log(res);
+       },
+       err => {
+         console.log('Error occurred');
+       }
+     );
+ }*/
+
+  sendValue(operator: string, operand1: string, operand2: string) {
+    operator = this.operation[1];
+    operand1 = this.operation[0];
+    operand2 = this.operation[2];
+    console.log('operator = ' + operator);
+    switch (operator) {
+      case '*':
+        operator = 'multiply';
+        break;
+      case '+':
+        operator = 'add';
+        break;
+      case '-':
+        operator = 'subtract';
+        break;
+      case '/':
+        operator = 'divide';
+        break;
+    }
+    this.http.get('http://localhost:8080/api/v1/calculator/' + operator + '/' + operand1 + '/' + operand2)
+      .subscribe( (response: any) => {
+          this.loop();
+          this.activeBuildingNumber = response.answer;
+          console.log(this.activeBuildingNumber);
+          this.display = this.activeBuildingNumber;
+          this.operation[0] = response.answer;
+        });
+  }
+  sendPower(operator: string, operand1: string) {
+    operand1 = this.operation[0];
+      operator = 'square';
+    this.http.get('http://localhost:8080/api/v1/calculator/' + operator + '/' + operand1)
+      .subscribe( (response: any) => {
+          this.loop();
+          this.activeBuildingNumber = response.answer;
+          this.operation[0] = this.activeBuildingNumber;
+          this.display = this.operation[0];
+          // console.log(this.operation[0]);
+        }
+      );
+  }
+  sendRoot(operator: string, operand1: string) {
+    operand1 = this.operation[0];
+    operator = 'squareroot';
+    this.http.get('http://localhost:8080/api/v1/calculator/' + operator + '/' + operand1)
+      .subscribe( (response: any) => {
+          this.loop();
+          this.activeBuildingNumber = response.answer;
+          this.operation[0] = this.activeBuildingNumber;
+          this.display = this.operation[0];
+          // console.log(this.operation[0]);
+        }
+      );
+  }
+
+
+
+
+
+
+
+
   buildNumber(num: string): void {
     this.activeBuildingNumber += num;
+
     // if operator is defined, set second variable
     if (this.operation[1].length) {
       this.operation[2] = this.activeBuildingNumber;
     } else {
       this.operation[0] = this.activeBuildingNumber;
-        // this.subDisplay = '';
-    }
+      // this.subDisplay = '';
 
+    }
     this.renderDisplay();
   }
-
   // render display
   renderDisplay(): void {
     this.display = this.operation.join(' ');
@@ -60,32 +141,32 @@ export class CalculatorComponent implements OnInit {
   selectOperator(operator: string): void {
     if (!this.operation[0].length) {
       this.displayError();
-      // this.subDisplay = 'Enter number before operation';
       return;
     }
     this.operation[1] = operator;
+
     this.activeBuildingNumber = '';
     this.renderDisplay();
   }
-
-  showResult(): void {
-    if (this.confirmInputs() ) {
-      const val = this.calculateResult();
-
-      this.display = '' + val;
-      // this.subDisplay = this.operation.join(' ');
-      this.activeBuildingNumber = this.display;
-        console.log(this.activeBuildingNumber);
-        this.loop();
-      this.operation[0] = this.activeBuildingNumber;
-      this.display = this.activeBuildingNumber;
-
-    }
-  }
+  // showResult(): void {
+  //   if (this.confirmInputs() ) {
+  //     const val = this.calculateResult();
+  //
+  //     this.display = '' + val;
+  //     // this.subDisplay = this.operation.join(' ');
+  //     this.activeBuildingNumber = this.display;
+  //     console.log(this.activeBuildingNumber);
+  //     this.loop();
+  //     this.operation[0] = this.activeBuildingNumber;
+  //     this.display = this.activeBuildingNumber;
+  //
+  //   }
+  // }
   loop(): void {
     this.display = '';
     // this.subDisplay = '';
     this.operation = ['', '', ''];
+
   }
   resetOperation(): void {
     this.operation = ['', '', ''];
@@ -115,20 +196,16 @@ export class CalculatorComponent implements OnInit {
     return true;
   }
 
-  calculateResult(): number {
-    switch (this.operation[1]) {
-      case '*':
-        return parseFloat(this.operation[0]) * parseFloat(this.operation[2]);
-      case '+':
-        return parseFloat(this.operation[0]) + parseFloat(this.operation[2]);
-      case '-':
-        return parseFloat(this.operation[0]) - parseFloat(this.operation[2]);
-      case '/':
-        return parseFloat(this.operation[0]) / parseFloat(this.operation[2]);
-    }
-
-
-  }
-
-
+  // calculateResult(): number {
+  //   switch (this.operation[1]) {
+  //     case '*':
+  //       return parseFloat(this.operation[0]) * parseFloat(this.operation[2]);
+  //     case '+':
+  //       return parseFloat(this.operation[0]) + parseFloat(this.operation[2]);
+  //     case '-':
+  //       return parseFloat(this.operation[0]) - parseFloat(this.operation[2]);
+  //     case '/':
+  //       return parseFloat(this.operation[0]) / parseFloat(this.operation[2]);
+  //   }
+  // }
 }
